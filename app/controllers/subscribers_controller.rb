d3052c8c2589dd883ec3ece8f1b4ddce26cafb23
Christina Thompson <c.thompsonms@gmail.com>
2014-08-31 13:42:10 -0700

    create subscribers

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -3,5 +3,19 @@ class SubscribersController < ApplicationController
   def new
     @subscriber = Subscriber.new
   end
-  
+
+  def create
+    @subscriber = Subscriber.create(subscriber_params)
+    if @subscriber.save
+      redirect_to root_path, notice: "Thanks for subscribing!"
+    else
+      render 'form'
+    end
+  end
+
+  private
+  def subscriber_params
+    params.require(:subscriber).permit(:email)
+  end
+
 end
